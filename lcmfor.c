9547013ee48227d135fc0eabd81d47757bacfd86
Tanu Mourya <abhagakt236@gmail.com>
2025-09-04 19:30:39 +0530

    Add files via upload

---
+++ lcmfor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,lcm;
+   
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        fact *= i;
+     }
+     printf("Factorial of %d is: %d",n,fact);
+     return 0;
+}
